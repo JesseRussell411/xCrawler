dcb48d5174dc086e8c96c6c11ac5f9f988846291
jesserussell <jesserussell411@gmail.com>
2021-11-09 21:53:12 -0700

    though some more shit at the wall

---
+++ locomotion/Locomotion.ino
@@ -0,0 +1,32 @@
+/*
+Work in Progress
+----------------
+
+This code isn't even functional yet. It's just spaughetti through at the wall
+in the hopes that some of it sticks.
+*/
+
+int powerLeft = 0;
+int powerRight = 0;
+
+typedef struct Power{
+    int value;
+    int max;
+    int min;
+    int decay;
+    int shrink;
+} Power;
+
+void p_decay(const Power *power) {
+    if (0 != power->value){
+        
+    }
+}
+
+void setup(){
+
+}
+
+void loop(){
+
+}
