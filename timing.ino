712b1a704f581f5399dc17d5f0d5245e80749519
jesserussell <jesserussell411@gmail.com>
2021-11-12 16:43:40 -0700

    timing barely functional now!

--- timing.ino
+++ timing.ino
@@ -1,47 +1,54 @@
 class Interval{
 private:
-  bool (*action)();
+  int (*action)();
   unsigned long interval;
   unsigned long start;
 public:
-  Interval(bool (*action)(), unsigned long interval){
+  Interval() = default;
+  Interval(int (*action)(), unsigned long interval){
     this->interval = interval;
     this->action = action;
+    this->start = millis();
   }
 
-  bool update(){
+  int update(){
     unsigned long now = millis();
     unsigned long ellapsed = now - start;
     if (ellapsed >= interval){
-      return action();
       start = now - (ellapsed - interval);
-    } else return false;
+      return action();
+    } else return 0;
   }
-}
-
-class Timing{
-private:
-  
-}
+};
 
 
+Interval interval;
+int ledState = LOW;
 
 
-void (*todo1)();
-void (*todo2)();
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
-  todo1 = [](){digitalWrite(LED_BUILTIN, HIGH);};
-  todo2 = [](){digitalWrite(LED_BUILTIN, LOW);};
+
+  interval = Interval([](){
+    if(ledState == LOW){
+      ledState = HIGH;
+      Serial.println("H");
+    }else{
+      ledState = LOW;
+      Serial.println("L");
+    }
+    
+
+    digitalWrite(LED_BUILTIN, ledState);
+
+    return 0;
+  }, 900);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  todo1();
-  delay(1000);
-  todo2();
-  delay(1000);
+  interval.update();
 }
